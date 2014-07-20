a9aa44aa4a4b36d2f82b435abd3ec2dfa3750b6d
babsgosgens <babsgosgens@gmail.com>
2014-07-20 11:38:49 +0200

    Remove Persistent Grid source

--- README.md
+++
@@ -1,5 +0,0 @@
-# The Persistent Grid
-
-Persistent Grid is pre-alpha.
-
-More info can be found here: [http://www.persistentgrid.com](http://www.persistentgrid.com)
--- lib/persistentgrid.rb
+++
@@ -1,4 +0,0 @@
-require 'compass'
-Compass::Frameworks.register('persistentgrid',
-                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'scss'),
-                             :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
