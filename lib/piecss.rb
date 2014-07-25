ff6df394f8cef8a02f2f40eb43fabf6cad6086d2
babsgosgens <babsgosgens@gmail.com>
2014-07-25 10:08:26 +0200

    Add pattern. Update import paths.

--- lib/piecss.rb
+++ lib/piecss.rb
@@ -1,4 +1,6 @@
 require 'compass'
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('piecss',
-                            :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
-                            :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
+                                :path => extension_path,
+                                :stylesheets_directory => File.join(extension_path, 'sass'),
+                                :templates_directory => File.join(extension_path, 'templates'))
--- piecss.gemspec
+++ piecss.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   # Release Specific Information
   s.licenses = ['MIT']
-  s.version = '0.1.5'
+  s.version = '0.1.6'
   s.date = '2014-07-20'
 
   # Gem Details
